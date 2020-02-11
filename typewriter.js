7cec0fa1c4a73e9979825515d599e2a472d151cc
Nick Pope <psydpope@gmail.com>
2020-02-11 20:23:17 +0000

    worked with @alextheprogrammer21

--- typewriter.js
+++ typewriter.js
@@ -1,5 +1,9 @@
-const sentence = "hello there from lighthouse labs";
+let sentence = "hello there from lighthouse labs";
 
-for (const char of sentence) {
-  process.stdout.write(char);
+sentence += "\n";
+
+for (let i = 0; i < sentence.length; i++) {
+  setTimeout(() => {
+    process.stdout.write(sentence[i]);
+  }, 100 + i * 100);
 }
